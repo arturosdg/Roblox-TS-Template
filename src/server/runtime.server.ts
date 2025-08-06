b4b35a559fae2a3db4968b45388b1fc24cd9d438
Arturo Sanchez <31703613+arturosdg@users.noreply.github.com>
2025-08-06 18:24:03 +0200

    chore: make tests work

--- src/server/runtime.server.ts
+++ src/server/runtime.server.ts
@@ -1,7 +1,15 @@
 import { Flamework } from "@flamework/core";
+import TestEZ from "@rbxts/testez";
+import {RunService} from "@rbxts/services";
 
 Flamework.addPaths("src/server/components");
 Flamework.addPaths("src/server/services");
 Flamework.addPaths("src/shared/components");
 
 Flamework.ignite();
+
+
+export const IS_DEV = RunService.IsStudio();
+if (IS_DEV) {
+    TestEZ.TestBootstrap.run([script.Parent!]);
+}
