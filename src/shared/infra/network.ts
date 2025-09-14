03d243dc1fddc4375ae304c4f359e06dcefff03f
Arturo Sanchez <31703613+arturosdg@users.noreply.github.com>
2025-09-14 17:52:49 +0200

    chore: fix mistake

--- src/shared/infra/network.ts
+++ src/shared/infra/network.ts
@@ -11,9 +11,7 @@ interface ServerEvents {
 	toggleSetting: (setting: Setting) => void;
 }
 
-interface ServerFunctions {
-	F;
-}
+interface ServerFunctions {}
 
 interface ClientEvents {
 	reflex: {
